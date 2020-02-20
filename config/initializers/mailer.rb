d63676c878ddcb150d55ffdd33ac3cfb88c36c9c
Umer Bilal <omerpucit@gmail.com>
2020-02-20 22:56:56 +0500

    fixed signup issue

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address:        'smtp.gmail.com', # default: localhost
+  address:        'mail.smtp2go.com', # default: localhost
   port:           587,                  # default: 25
-  domain: 'mail.google.com',
-  user_name:      'sellist.beryl@gmail.com',
-  password:       'Berylsystems@123',
+  #domain: 'mail.google.com',
+  user_name:      'omerpucit@technodevs.com',
+  password:       'VqsfKEZPHdNB',
   authentication: :plain,                 # :plain, :login or :cram_md5
   enable_starttls_auto: true
 }
